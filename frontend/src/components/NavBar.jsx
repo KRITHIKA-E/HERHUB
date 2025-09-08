2ad49231557023588d978816b22d624f4ded62f3
KRITHIKA-E <230698.it@rmkec.ac.in>
2025-09-08 14:34:30 +0530

    new changes

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../public/logo.jpg';
 
 const NavBar = () => {
   return (
     <nav>
-      <Link to="/"><img src={logo} alt="HER HUB Logo" /></Link>
+      <Link to="/"><img src="/logo.jpg" alt="HER HUB Logo" style={{ height: '50px' }} /></Link>
       <div>
         <Link to="/">Home</Link>
         <Link to="/login">Login</Link>
